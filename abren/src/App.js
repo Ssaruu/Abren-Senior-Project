a9f2e14c7650fdaf323d7f35f05c26361d50a515
NaniBer <gdgr2010@gmail.com>
2023-12-14 09:56:27 +0300

    Dashboard page added

--- abren/src/App.js
+++ abren/src/App.js
@@ -1,16 +1,49 @@
-import './App.css';
-import { Routes, Route} from "react-router-dom";
-import Form from "./scenes/form";
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Form from "./scenes/form/form";
+//import Login from "./scenes/form/login";
+// import Dashboard from "./scenes/dashboard";
+import DashboardAdmin from "./scenes/dashboard-admin";
+// import Topbar from "./scenes/global/Topbar";
+// import Sidebar from "./scenes/global/Sidebar";
+// import SidebarAdmin from "./scenes/global/SidebarAdmin";
+// import Createproject from "./scenes/createproject";
+// import Createteamaccount from "./scenes/createteamaccount";
+// import Calendar from "./scenes/calendar";
+// import Kanban from "./scenes/kanban";
+// import Team from "./scenes/team";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import SidebarAdmin from "./scenes/global/SideBarAdmin";
+import Topbar from "./scenes/global/Topbar";
+
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <Routes>
-      <Route path="/" element={<Form />}></Route>
-
-      </Routes>
-
-      <p>hello</p>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <SidebarAdmin />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<DashboardAdmin />}></Route>
+              <Route path="/form" element={<Form />}></Route>
+              {/*<Route path="/calendar" element={<Calendar />}></Route>*/}
+              {/* <Route path="/kanban" element={<Kanban />}></Route> */}
+              {/* <Route path="/team" element={<Team />}></Route> */}
+              {/* <Route path="/createproject" element={<Createproject />}></Route> */}
+              {/* <Route
+                path="/createteamaccount"
+                element={<Createteamaccount />}
+              ></Route> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
